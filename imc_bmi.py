503e6c8a9d78e82026b298d6470230ba4f9175ff
Duca Barros <80001001+DucaBarros@users.noreply.github.com>
2025-06-05 17:30:59 -0300

    Updated code
    Formatado Float do BMI com 1 casa decimal

--- imc_bmi.py
+++ imc_bmi.py
@@ -15,34 +15,32 @@ bmi_result = bmi(weight, height)
 
 # If BMI < 16 show the result "Severe Thinness"
 if bmi_result < 16:
-    print('Severe Thinness')
+    print(f'Your BMI is: {bmi_result:.1f} and you are Severe Thinness')
 
 # If BMI >= 16 and BMI <= 17 show the result "Moderate Thinness"
 elif bmi_result >= 16 and bmi_result <= 17:
-    print('Moderate Thinness')
+    print(f'Your BMI is: {bmi_result:.1f} and you are Moderate Thinness')
 
 # If BMI > 17 and BMI <= 18.5 show the result "Mild Thinness"
 elif bmi_result > 17 and bmi_result <= 18.5:
-    print('Mild Thinness')
+    print(f'Your BMI is: {bmi_result:.1f} and you are Mild Thinness')
 
 # If BMI > 18.5 and BMI <= 25 show the result "Normal"
 elif bmi_result > 18.5 and bmi_result <= 25:
-    print('Normal')
+    print(f'Your BMI is: {bmi_result:.1f} and you are Normal')
 
 # If BMI > 25 and BMI <= 30 show the result "Overweight"
 elif bmi_result > 25 and bmi_result <= 30:
-    print('Overweight')
+    print(f'Your BMI is: {bmi_result:.1f} and you are Overweight')
 
 # If BMI > 30 and BMI <= 35 show the result "Obese Class I"
 elif bmi_result > 30 and bmi_result <= 35:
-    print('Obese Class I')
+    print(f'Your BMI is: {bmi_result:.1f} and you are Obese Class I')
 
 # If BMI > 35 and BMI <= 40 show the result "Obese Class II"
 elif bmi_result > 35 and bmi_result <= 40:
-    print('Obese Class II')
+    print(f'Your BMI is: {bmi_result:.1f} and you are Obese Class II')
 
 # If BMI > 40 show the result "Obese Class III"
 elif bmi_result > 40:
-    print('Obese Class III')
-
-print(f'Your BMI is: {bmi_result}')
+    print(f'Your BMI is: {bmi_result:.1f} and you are Obese Class III')
