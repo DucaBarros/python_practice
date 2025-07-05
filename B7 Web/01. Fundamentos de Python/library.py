2675b94d6339e546a12c848ae35a69fb0c8b12ed
Duca Barros <80001001+DucaBarros@users.noreply.github.com>
2025-07-04 23:14:37 -0300

    Delete B7 Web directory

--- B7 Web/01. Fundamentos de Python/library.py
+++
@@ -1,9 +0,0 @@
-posts = [    {"title": "DragonBall Z", "author": "Akira Toriyama"},    
-             {"title": "Yu Yu Hakusho", "author": "Yoshihiro Togashi"},
-             {"title": "Naruto Shippuden", "author": "Masashi Kishimoto"}
-]   
-
-for post in posts:    
-    print(post["title"])    
-    print(f"Autor: {post["author"]}")    
-    print("------------------")
--- B7 Web/01. Fundamentos de Python/teste.py
+++
@@ -1,4 +0,0 @@
-my_name = input('Digite seu nome: ').title()
-
-print(my_name)
-print(type(my_name))
